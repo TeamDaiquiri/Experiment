65a6e1eee907672f1ae5ea361a3eb23000cf6aa3
TanyaRan <tatyanaran@gmail.com>
2016-08-11 12:49:20 +0300

    changes in main.js
    add background canvas 
    TODO: add moving stars

--- BigShip/js/main.js
+++ BigShip/js/main.js
@@ -5,12 +5,25 @@ window.addEventListener('load', function() {
     var WIDTH = 1200,
         HEIGHT = 700;
 
+    var bgCanvas = document.getElementById('background-canvas'),
+        bgContext = bgCanvas.getContext('2d');
+
+    bgCanvas.width = WIDTH;
+    bgCanvas.height = HEIGHT;
+
     var shipCanvas = document.getElementById('ship-canvas'),
         shipContext = shipCanvas.getContext('2d'),
         shipImg = document.getElementById('ship');
 
     shipCanvas.width = WIDTH;
     shipCanvas.height = HEIGHT;
+
+    var enemyCanvas = document.getElementById('enemy-canvas'),
+        enemyContext = enemyCanvas.getContext('2d'),
+        enemyImg = document.getElementById('enemy');
+
+    enemyCanvas.width = WIDTH;
+    enemyCanvas.height = HEIGHT;
 
     var ship = createGameObject({
       spritesheet: shipImg,
@@ -26,10 +39,27 @@ window.addEventListener('load', function() {
     });
 
     var singleShipWidth = ship.width/ship.colsCount,
-        singleShipHeight = ship.height/ship.rowsCount;
-
-    var shipStartX = WIDTH/2 - singleShipWidth/2,
+        singleShipHeight = ship.height/ship.rowsCount,
+        shipStartX = WIDTH/2 - singleShipWidth/2,
         shipStartY = HEIGHT - singleShipHeight - 10;
+
+    var enemy = createGameObject({
+      spritesheet: enemyImg,
+      context: enemyContext,
+      width: enemyImg.width,
+      height: enemyImg.height,
+      colsCount: 3,
+      rowsCount: 2,
+      colIndex: 0,
+      rowIndex: 0,
+      loopTicksCount: 0,
+      loopTicksPerFrame: 25,
+    });
+
+    var singleEnemyWidth = enemy.width/enemy.colsCount,
+        singleEnemyHeight = enemy.height/enemy.rowsCount,
+        enemyStartX = WIDTH/2,
+        enemyStartY = 10;
 
     function gameLoop() {
 
@@ -37,7 +67,13 @@ window.addEventListener('load', function() {
         .render(
           {x: shipStartX, y: shipStartY},
           {x: shipStartX, y: shipStartY})
-        .update();
+        .update(true);
+
+      enemy
+        .render(
+          {x: enemyStartX, y: enemyStartY},
+          {x: enemyStartX, y: enemyStartY})
+        .update(false);
 
         window.requestAnimationFrame(gameLoop);
     }
